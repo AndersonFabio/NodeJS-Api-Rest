3edf061b64e3ce7c93e830ca21017f9ff711d74f
Anderson Fábio <andersonfabio.1976@gmail.com>
2021-03-15 11:11:23 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://blocomariodean01:paco1701@mongodb.blocomariodeandrade.com.br/blocomariodean01';
+const url = 'mongodb://blocomariodean01:@mongodb.blocomariodeandrade.com.br/blocomariodean01';
 
 mongoose.connect(url, {useNewUrlParser:true});
 
